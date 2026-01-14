8f18ed503c51301a56ef78c2f10a52c5754c2fa9
Lanna Check <lannacheck@gmail.com>
2026-01-14 18:50:42 -0500

    Started Question 2

--- p5-rev-template/p5-rev-template/js/script-Q2.js
+++ p5-rev-template/p5-rev-template/js/script-Q2.js
@@ -13,20 +13,12 @@ function setup() {
 
 function draw() {
     background(0);
-    noStroke();
-    // First Circle
+}
+
+function drawEllipse(x, y, w, h, r, g, b) {
     push();
+    noStroke();
     fill(225, 100, 100)
     ellipse(20, 15, 15);
-    pop();
-    // Second Circle
-    push();
-    fill(225, 150, 100)
-    ellipse(50, 50, 40);
-    pop();
-    // Third Circle
-    push();
-    fill(225, 200, 100)
-    ellipse(90, 120, 60);
     pop();
 }
