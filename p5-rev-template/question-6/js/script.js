cb44832388f5fd59876df024857a931c5ca3249d
Lanna Check <lannacheck@gmail.com>
2026-01-15 17:49:02 -0500

    Finished question 6

---
+++ p5-rev-template/question-6/js/script.js
@@ -0,0 +1,58 @@
+/**
+ * TASK 1
+ * Lanna Check
+ */
+
+"use strict";
+
+let test = {
+    fill: "#ffff",
+    size: 28,
+    x: undefined,
+    y: undefined,
+    name: "test"
+};
+
+let numX = 60;
+let numY = 40;
+let numSpace = 28;
+/**
+ * Creates the canvas and draws text and numbers
+ */
+function setup() {
+    console.log("go");
+    createCanvas(500, 500);
+    background(0);
+    test.x = width / 2;
+    test.y = height / 2;
+    // Numbers from 0-9
+    for (let i = 0; i < 10; i++) {
+        push();
+        fill(test.fill);
+        textSize(test.size);
+        textAlign(CENTER, CENTER);
+        text(i, i * numSpace + numX, numY);
+        pop();
+    }
+
+    // Numbers from 0-15
+    for (let i = 0; i <= 15; i++) {
+        push();
+        fill(test.fill);
+        textSize(test.size);
+        textAlign(CENTER, CENTER);
+        text(i, numX, i * numSpace + numY);
+        pop();
+    }
+    // Draw the test text
+    push();
+    fill(test.fill);
+    textSize(test.size);
+    textAlign(CENTER, CENTER);
+    text(test.name, test.x, test.y);
+    pop();
+}
+
+function draw() {
+
+}
