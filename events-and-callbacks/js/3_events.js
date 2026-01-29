a2665a812110a61c9a11581f7ad6611b666f8e42
Lanna Check <lannacheck@gmail.com>
2026-01-28 19:40:26 -0500

    Used about template strings instead of using plus signs and stuff

--- events-and-callbacks/js/3_events.js
+++ events-and-callbacks/js/3_events.js
@@ -5,8 +5,26 @@ function setup() {
     introSection.addEventListener("click", mouseClickCallBack)
     // When you add mouseClickCallBack with paranthesis "()" it immediately calls the function. But without the parantheis makes sure it only calls when the introSection is clicked
 
+    let s1 = document.querySelector("#s1");
+    s1.addEventListener("click", mouseClickCallBack);
+
 }
 
-function mouseClickCallBack() {
-    console.log("clicked");
+function mouseClickCallBack(eventObj) {
+    //console.log("clicked");
+    // console.log(this)
+    console.log(eventObj)
+    // "this" refers to the elements that triggered the event listener. So when the intro is clicked it will return the intro section elements. Useful way to access who triggered the click
+    this.style.bacground = "blue"
+    //eventObj.target.style.background = "blue" => This changes the color of the specific element being clicked Ex: the paragraph being clicked not the whole element.
+    let idOfThis = this.id
+    //console.log(document.querySelector(`#${idOfThis} p`))
+    // Template strings => `#${variable} attribute` have to use back quotation marks
+    let classToAdd = `${idOfThis}-section-active`
+    // The class name will become the name of the id + -section-active
+    this.classList.add(classToAdd);
+}
+
+function mouseClicks1CallBack() {
+    console.log("s1 clicked")
 }
