ecaae926f7929161e2e90f5463d07b79f3d60b70
Lanna Check <lannacheck@gmail.com>
2026-02-04 18:35:33 -0500

    Finished key events started timer events

--- WEEK-4-START/WEEK-4-START/js/week4.js
+++ WEEK-4-START/WEEK-4-START/js/week4.js
@@ -2,8 +2,42 @@ window.onload = setup;
 function setup() {
   console.log("keys");
   window.addEventListener("keydown", keyHandler);
+  window.addEventListener("keyup", keyHandlerUp);
+  window.setTimeout(addTimeoutText, 2000);
+  window.setInterval(addTimeInterval, 2000);
 
+  function addTimeoutText() {
+    let parent = document.querySelector("#parent");
+    parent.innerHTML += "NEW TEXT"
+  }
+  function addTimeInterval() {
+    let parent = document.querySelector("#parent");
+    parent.innerHTML += "NEW TEXT FOR INTERVAL"
+  }
+
+  function keyHandlerUp(event) {
+    if (event.key === " ") {
+      document.querySelector("#boxB").style.background = "blue";
+    }
+  }
+
+  let speedX = 5;
   function keyHandler(event) {
-    console.log(event.key)
+
+    // ParseInt is used to change the text from the style to an interger so the SpeedX can be added to it 
+    if (event.key === "ArrowRight") {
+      document.querySelector("#boxA").style.left = parseInt(document.querySelector("#boxA").style.left) + speedX + "px";
+    }
+    if (event.key === "ArrowLeft") {
+      document.querySelector("#boxA").style.left = parseInt(document.querySelector("#boxA").style.left) - speedX + "px";
+    }
+    if (event.key === " ") {
+      document.querySelector("#boxB").style.background = "orange";
+    }
+    // else {
+    //   console.log(event.key);
+    //   document.querySelector("#textContainer").textContent += `${event.key} `;
+    // }
+
   }
 }
