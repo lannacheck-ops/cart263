edcaba99086e934d0aed3755eaf5ee9a0b068e73
Lanna Check <lannacheck@gmail.com>
2026-03-08 05:22:33 -0400

    Added all the images to the DOM
    I will animate the character and the light next

--- project-1/js/Box.js
+++ project-1/js/Box.js
@@ -1,15 +1,44 @@
 window.onload = function () {
-  let box = {
-    background: document.createElement("img"),
-
+  let layout = {
+    background: {
+      div: document.createElement("div"),
+      image: document.createElement("img")
+    },
+    box: document.createElement("img"),
+    grid: document.createElement("img"),
+    light: document.createElement("img"),
+    dispenser: document.createElement("img"),
+    heart: document.createElement("img"),
+    button: document.createElement("img"),
+    character: document.createElement("img")
   }
 
   // Add to DOM
-  function renderBox() {
-    box.background.src = "/images/background.png"
-    box.background.classList.add("contain");
-    document.getElementsByTagName("main")[0].appendChild(box.background);
+  function renderlayout() {
+    // bg
+    layout.background.div.classList.add("layout");
+    layout.background.image.src = "/images/background.png";
+    layout.background.div.appendChild(layout.background.image);
+    document.getElementsByTagName("main")[0].appendChild(layout.background.div);
+    //box
+    layout.box.src = "/images/box.png";
+    document.querySelector(".layout").appendChild(layout.box);
+    //electric grid
+    layout.light.src = "images/light-off.png";
+    document.querySelector(".layout").appendChild(layout.light);
+    //dispenser
+    layout.dispenser.src = "images/dispenser.png";
+    document.querySelector(".layout").appendChild(layout.dispenser);
+    //heart
+    layout.heart.src = "images/heart.png";
+    document.querySelector(".layout").appendChild(layout.heart);
+    //button
+    layout.button.src = "images/button-unpressed.png";
+    document.querySelector(".layout").appendChild(layout.button);
+    //character
+    layout.character.src = "images/character-sit.png";
+    document.querySelector(".layout").appendChild(layout.character);
   }
-  renderBox();
+  renderlayout();
 }
 
