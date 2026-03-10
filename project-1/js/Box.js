9dec58751350c35432369289c97b21b243728251
Lanna Check <lannacheck@gmail.com>
2026-03-10 04:09:23 -0400

    Added the button flex box where the buttons are going to be placed

--- project-1/js/Box.js
+++ project-1/js/Box.js
@@ -1,47 +1,100 @@
 window.onload = function () {
+  let lightOn = false;
+  let charState = "sit";
   let layout = {
     background: {
       div: document.createElement("div"),
-      image: document.createElement("img")
+      image: document.createElement("img"),
     },
-    box: document.createElement("img"),
-    grid: document.createElement("img"),
-    light: document.createElement("img"),
-    dispenser: document.createElement("img"),
-    heart: document.createElement("img"),
-    button: document.createElement("img"),
-    character: document.createElement("img")
-  }
+    box: {
+      el: document.createElement("img"),
+    },
+    grid: {
+      el: document.createElement("img"),
+    },
 
-  // Add to DOM
+    lights: {
+      el: document.createElement("img"),
+      // For the sources I'm thinking of adding them to an array so its  stored in just one variable
+      offSrc: "/images/light-on.png",
+      onSrc: "/images/light-on.png",
+      srcPaths: ["images/light-off.png", "/images/light-on.png"],
+      image: ""
+    },
+    dispenser: {
+      el: document.createElement("img"),
+    },
+    heart: {
+      el: document.createElement("img"),
+    },
+    button: {
+      el: document.createElement("img"),
+    },
+    character: {
+      el: document.createElement("img"),
+      srcPaths: ["images/character-sit.png", "images/character-stand.png", "images/character-push.png", "images/character-pick.png"],
+      image: ""
+    },
+
+  };
+  /**
+   * Changes light to On or Off
+   */
+  function updateLight() {
+    if (lightOn) {
+      layout.lights.image = layout.lights.srcPaths[1]
+    }
+    else {
+      layout.lights.image = layout.lights.srcPaths[0]
+    }
+  }
+  /**
+   * Add images to the DOM
+   */
   function renderlayout() {
     // bg
     layout.background.div.classList.add("layout");
     layout.background.image.src = "images/background.png";
     layout.background.div.appendChild(layout.background.image);
     document.getElementsByTagName("main")[0].appendChild(layout.background.div);
-    //box
-    layout.box.src = "images/box.png";
-    document.querySelector(".layout").appendChild(layout.box);
-    //electric grid
-    layout.grid.src = "images/electric-grid.png";
-    document.querySelector(".layout").appendChild(layout.grid);
-    //light
-    layout.light.src = "images/light-off.png";
-    document.querySelector(".layout").appendChild(layout.light);
-    //dispenser
-    layout.dispenser.src = "images/dispenser.png";
-    document.querySelector(".layout").appendChild(layout.dispenser);
-    //heart
-    layout.heart.src = "images/heart.png";
-    document.querySelector(".layout").appendChild(layout.heart);
-    //button
-    layout.button.src = "images/button-unpressed.png";
-    document.querySelector(".layout").appendChild(layout.button);
-    //character
-    layout.character.src = "images/character-sit.png";
-    document.querySelector(".layout").appendChild(layout.character);
-  }
-  renderlayout();
-}
+    //box // right here 
+    layout.box.el.src = "images/box.png";
+    layout.box.el.id = "box"
 
+    /**
+       * LEON GOONER #1 AREA ❤️
+       */
+
+    document.querySelector(".layout").appendChild(layout.box.el);
+    //electric grid 
+    layout.grid.el.src = "images/electric-grid.png";
+    layout.grid.el.id = "grid"
+    document.querySelector(".layout").appendChild(layout.grid.el);
+
+    // light off
+    layout.lights.el.src = layout.lights.image;
+    layout.lights.el.id = "light"
+    document.querySelector(".layout").appendChild(layout.lights.el);
+    /**
+     * LEON GOONER #2 AREA 😋 // I think we should leave these comments in they're so funny LMFAO PLSKDSJDJ
+     */
+    //dispenser
+    layout.dispenser.el.src = "images/dispenser.png";
+    layout.dispenser.id = "dispenser"
+    document.querySelector(".layout").appendChild(layout.dispenser.el);
+    //heart
+    layout.heart.el.src = "images/heart.png";
+    layout.heart.id = "heart"
+    document.querySelector(".layout").appendChild(layout.heart.el);
+    //button
+    layout.button.el.src = "images/button-unpressed.png";
+    layout.button.id = "push-button"
+    document.querySelector(".layout").appendChild(layout.button.el);
+    //character
+    layout.character.el.src = "images/character-sit.png";
+    layout.character.el.id = "char"
+    document.querySelector(".layout").appendChild(layout.character.el);
+  }
+  updateLight();
+  renderlayout();
+}; // Hey chat
