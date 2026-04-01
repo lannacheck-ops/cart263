1fa83823320a1eb14ceb7560359eb2ab339b68e6
Lanna Check <lannacheck@gmail.com>
2026-04-01 18:24:20 -0400

    made spheres turn blue when they overlap the ray caster

---
+++ three-js-two-start/rayCastTest.js
@@ -0,0 +1,88 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+
+const scene = new THREE.Scene()
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const canvas = document.querySelector('canvas#three-ex')
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3;
+scene.add(camera)
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+const controls = new OrbitControls(camera, canvas)
+
+const object1 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object1.position.x = -2
+
+const object2 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+object2.position.x = 2
+
+
+const object3 = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 16, 16),
+    new THREE.MeshBasicMaterial({ color: '#ff0000' })
+)
+
+scene.add(object1, object2, object3)
+// Forces objects to have updated positions to check the intersection positions cuz object positions havent updated till we render them
+object1.updateMatrixWorld()
+object2.updateMatrixWorld()
+object3.updateMatrixWorld()
+
+// Raycast
+const raycaster = new THREE.Raycaster()
+//ray will start somewhere on left of the spheres
+const rayOrigin = new THREE.Vector3(- 3, 0, 0)
+//right (positive x) pointing to the right
+const rayDirection = new THREE.Vector3(10, 0, 0)  //reduce magnitude BUT keep direction
+console.log(rayDirection.length())
+//set direction only (has length ==1) normalizing means itll have a length of 1
+rayDirection.normalize()
+console.log(rayDirection.length())
+raycaster.set(rayOrigin, rayDirection) //raycaster has been oriented
+
+//cast a ray - check intersection with ONLY object 1
+// const intersect = raycaster.intersectObject(object1)
+// console.log(intersect)
+
+//cast a ray - check intersection with obj1, obj2 and obj 3 
+// const intersects = raycaster.intersectObjects([object1, object2, object3])
+// console.log(intersects)
+
+window.requestAnimationFrame(animate);
+
+function animate(timer) {
+    controls.update();
+
+    object1.position.y = Math.sin(timer / 1000 * .5) * 3
+    object2.position.y = Math.sin(timer / 1000 * .4) * 3
+    object3.position.y = Math.sin(timer / 1000 * .3) * 3
+
+    const objectsToTest = [object1, object2, object3]
+    const intersects = raycaster.intersectObjects(objectsToTest)
+
+    for (const object of objectsToTest) {
+        object.material.color.set('#ff0000')
+    }
+
+    for (const intersect of intersects) {
+        // When object y position is 0 it turns blue
+        intersect.object.material.color.set('#0000ff')
+    }
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(animate);
+}
