be62b22ee482cea16568cd7cee8febb02f504142
Lanna Check <lannacheck@gmail.com>
2026-04-15 14:59:40 -0400

    was able to log face emotion and store it into a variable

--- face-recognition-api/script.js
+++ face-recognition-api/script.js
@@ -21,12 +21,29 @@ video.addEventListener('play', () => {
     const displaySize = { width: video.width, height: video.height }
     faceapi.matchDimensions(canvas, displaySize)
     setInterval(async () => {
+        // Stores the face detections
+        // An array that stores each face that has been detected
         const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        // Resizes the face detections based on the canvas size
         const resizedDetections = faceapi.resizeResults(detections, displaySize);
+        // console.log(resizedDetections.face.expressions);
+        // Clear the canvas 
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        // Draw bounding box
         faceapi.draw.drawDetections(canvas, resizedDetections);
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
         faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+        // Trying to log the facial expressions in real time
+        resizedDetections.forEach(face => {
+            const expressions = face.expressions;
+
+            const emotion = Object.keys(expressions).reduce((maxEmotion, currentEmotion) => {
+                return expressions[currentEmotion] > expressions[maxEmotion]
+                    ? currentEmotion
+                    : maxEmotion;
+            });
+            console.log(emotion); // "happy"
+        })
     }, 100)
 })
 // startVideo();
