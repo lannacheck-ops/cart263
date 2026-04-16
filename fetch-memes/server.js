6212285c27de7191c4e5e13a81e9f342b6d7bbbe
Lanna Check <lannacheck@gmail.com>
2026-04-16 01:42:00 -0400

    changed port const

--- fetch-memes/server.js
+++ fetch-memes/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 const API_KEY = process.env.API_KEY;
 
