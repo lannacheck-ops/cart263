7b462d0691717da5a051a628fb720ab548090260
Lanna Check <lannacheck@gmail.com>
2026-04-22 15:58:43 -0400

    Displayed the images now to make it appear as a carousel

--- surveillance-gallery-prototype/cat-inventory.js
+++ surveillance-gallery-prototype/cat-inventory.js
@@ -1,2 +1,17 @@
 let catInventory = JSON.parse(localStorage.getItem("cats")) || [];
 console.log(catInventory)
+
+// Get carousel div from html to be the parent div for the images to append to
+const parent = document.querySelector(".carousel");
+displayInventory();
+// Display the inventory
+function displayInventory() {
+    catInventory.forEach(cat => {
+        const imageDiv = document.createElement("div");
+        // Add class of card to each image
+        imageDiv.classList.add("card");
+        // Set image source
+        imageDiv.innerHTML += `<img src=${cat} />`
+        parent.appendChild(imageDiv);
+    });
+}
